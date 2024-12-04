b0fb746d9cacc94c2e0464922f198b1cdccbb90b
diegopiscitelli <info@diegopiscitelli.com>
2024-12-04 11:12:58 +0000

    set-up-and-basic-auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"solardrone-test","appId":"1:797682030341:web:3760817f72d7bee729496c","storageBucket":"solardrone-test.firebasestorage.app","apiKey":"AIzaSyB8eaHD7K3gW4tTMln2ZDigwSEXsRKS19E","authDomain":"solardrone-test.firebaseapp.com","messagingSenderId":"797682030341"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: false,
+	firebaseConfig : {
+		apiKey: "AIzaSyB8eaHD7K3gW4tTMln2ZDigwSEXsRKS19E",
+		authDomain: "solardrone-test.firebaseapp.com",
+		projectId: "solardrone-test",
+		storageBucket: "solardrone-test.firebasestorage.app",
+		messagingSenderId: "797682030341",
+		appId: "1:797682030341:web:3760817f72d7bee729496c"
+	}
+};
